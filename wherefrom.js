3e2106dae3fb89704ff02d8f7b03929a4e1b4f47
Ralph Cowling <acastofthousands@gmail.com>
2011-04-13 20:05:02 +0100

    adding in message for the non referrables

--- wherefrom.js
+++ wherefrom.js
@@ -1,11 +1,15 @@
 javascript:(function(){
     if(document.getElementById("_remover_"))return;
-    var r = document.referrer, div = document.createElement('div'), fEl = document.body.childNodes[0];
-    
+    var r = document.referrer,
+    div = document.createElement('div'), 
+    fEl = document.body.childNodes[0],
+    got = '<strong>Referrer: <a style="border-bottom: 1px dotted Snow;color: FloralWhite;text-decoration:none" href="'+r+'">'+r+'</a>',
+    notGot = 'Unable to find a referrer for this page.',
+    t = (r != null && r !=="") ? got : notGot;
+
     div.style.cssText ='z-index:1000;position:fixed;top:6px;right:6px;background:hotPink;color:white;display:block;width:300px;padding:10px;font-family:sans-serif;text-shadow:0px 1px 1px #000;font:12px sans-serif;opacity:.8;filter:alpha(opacity=80);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=80);"';
     
-    
-    div.innerHTML = '<a href="#" id="_remover_" style="float:right;padding:1px 6px;background:red;color:white;text-decoration:none;border-radius:10px;margin:-15px">x</a><strong>Referrer: <a style="border-bottom: 1px dotted Snow;color: FloralWhite;text-decoration:none" href="'+r+'">'+r+'</a></strong>';
+    div.innerHTML = '<a href="#" id="_remover_" style="float:right;padding:1px 6px;background:red;color:white;text-decoration:none;border-radius:10px;margin:-15px">x</a>'+t+'</strong>';
     
     document.body.insertBefore(div,fEl);
     
@@ -14,6 +18,6 @@ javascript:(function(){
     remover.onclick = function(){
         div.parentNode.removeChild(div);
         return false;
-    }
+    };
 
 })();
